50f95b32d271ee3c0fe45c4e76923da9830c7a89
ellinzhao <ellin.zhao@hotmail.com>
2018-04-14 09:43:40 -0700

    showing cross section

--- gen.py
+++ gen.py
@@ -15,16 +15,13 @@ class Img3D:
         self.k = k
         self.img_unfiltered, self.centers = self.gen_3d_points()
         # TODO: Might need to change sigma based on n.
-        # self.img_filtered = self.img_unfiltered
-        self.img_filtered = ndimage.gaussian_filter(self.img_unfiltered, sigma=10)
-
+        self.img_filtered = ndimage.gaussian_filter(self.img_unfiltered, sigma=1)
 
     def filter(self):
         filtered = np.empty((self.n, self.n, self.n))
         for i in range(self.n):
             filtered[i, :, :] = ndimage.gaussian_filter(self.img_unfiltered[i, :, :], sigma=4)
         return filtered
-
 
     def gen_3d_points(self):
         """
@@ -36,21 +33,24 @@ class Img3D:
         print("initializing image...")
         arr = np.zeros((self.n, self.n, self.n))
         centers = []
-        for _ in range(self.k):
-            a = np.random.randint(self.n // 4, 3 * self.n // 4)
-            b = np.random.randint(self.n // 4, 3 * self.n // 4)
-            c = np.random.randint(self.n//4, 3*self.n//4)
-            centers += [(a, b, c)]
-            r1 = np.random.randint(self.r // 2, self.r)
+        for i in range(self.k):
+            in_radius = [True]
+            while any(in_radius):
+                a = np.random.randint(4*self.r, self.n - 4*self.r)
+                b = np.random.randint(4*self.r, self.n - 4*self.r)
+                c = np.random.randint(4*self.r, self.n - 4*self.r)
+                in_radius = [(a - x) ** 2 + (b - y) ** 2 + (c - z) ** 2 <= 4*self.r ** 2 for x, y, z in centers]
+            centers.append((a, b, c))
+
+            #r1 = np.random.randint(self.r // 2, self.r)
+            r1 = self.r
 
             # TODO: fix center + radius selection
             #a, b, c = self.n // 2, self.n // 2, self.n // 2
-            #r1 = self.r
 
             z, y, x = np.ogrid[-c:self.n - c, -b:self.n - b, -a:self.n - a]
             mask = x*x + y*y + z*z <= r1*r1
             arr[mask] = 255
-            #arr = ndimage.gaussian_filter(arr, sigma=10)
         return arr, centers
 
     def show_img(self, filter_flag):
@@ -59,77 +59,65 @@ class Img3D:
         elif filter_flag == 1:
             plt.imshow(self.img_filtered[:, self.centers[0][0], :, :], cmap='gray', interpolation='nearest')
 
+    def x_section(self):
+        fig = plt.figure(figsize=(8, 4 * len(self.centers)))
 
-def generate_video(obj, vid=0, filtered=True, plane="xy"):
-    # clearing old files
-    folder = 'output'
-    """
-    for file in os.listdir(folder):
-        file_path = os.path.join(folder, file)
-        try:
-            if file[:5] != "video" and os.path.isfile(file_path):
-                os.unlink(file_path)
-        except Exception as e:
-            print(e)
-    """
-    if filtered:
-        img = obj.img_filtered
-    else:
-        img = obj.img_unfiltered
+        rows = len(self.centers)
+        for i in range(rows):
+            z, y, x = self.centers[i]
+            print(z, y, x)
+            axis = np.arange(0, self.n, 1)
 
-    # generating new pngs, mp4
-    for i in range(len(img)):
-        if plane == "xy":
-            plt.imshow(img[i], cmap=cm.Greys_r)
-        elif plane == "xz":
-            plt.imshow(img[:,i,:], cmap=cm.Greys_r)
-        elif plane == "yz":
-            plt.imshow(img[:,:,i], cmap=cm.Greys_r)
-        plt.savefig('output/file%02d.png' % i)
-    os.chdir('output')
-    subprocess.call([
-        'ffmpeg', '-framerate', '8', '-i', 'file%02d.png', '-r', '30', '-pix_fmt', 'yuv420p',
-        'video%d.mp4' % vid
-    ])
+            z_vals = self.img_filtered[:, y, x]
+            fig.add_subplot(rows, 3, 3 * i + 1)
+            plt.plot(axis, 10*z_vals)
 
-    # i think this is supposed to remove files but it does not.
-    for file_name in glob.glob("*.png"):
-        os.remove(file_name)
-    os.chdir('..')
+            y_vals = self.img_filtered[z, :, x]
+            fig.add_subplot(rows, 3, 3 * i + 2)
+            plt.plot(axis, 10*y_vals)
 
+            x_vals = self.img_filtered[z, y, :]
+            fig.add_subplot(rows, 3, 3 * i + 3)
+            plt.plot(axis, 10*x_vals)
 
-def generate_video_2(img, vid=0, plane="xy"):
-    # clearing old files
-    folder = 'output'
-    """
-    for file in os.listdir(folder):
-        file_path = os.path.join(folder, file)
-        try:
-            if file[:5] != "video" and os.path.isfile(file_path):
-                os.unlink(file_path)
-        except Exception as e:
-            print(e)
-    """
-    # generating new pngs, mp4
-    for i in range(len(img)):
-        if plane == "xy":
-            plt.imshow(img[i], cmap=cm.Greys_r)
-        elif plane == "xz":
-            plt.imshow(img[:,i,:], cmap=cm.Greys_r)
-        elif plane == "yz":
-            plt.imshow(img[:,:,i], cmap=cm.Greys_r)
-        plt.savefig('output/file%02d.png' % i)
-    os.chdir('output')
-    subprocess.call([
-        'ffmpeg', '-framerate', '8', '-i', 'file%02d.png', '-r', '30', '-pix_fmt', 'yuv420p',
-        'video%d.mp4' % vid
-    ])
+        plt.savefig("foo.png")
 
-    # i think this is supposed to remove files but it does not.
-    for file_name in glob.glob("*.png"):
-        os.remove(file_name)
-    os.chdir('..')
+    def generate_video(self, vid=0, filtered=True, plane="z"):
+        # clearing old files
+        folder = 'output'
+        """
+        for file in os.listdir(folder):
+            file_path = os.path.join(folder, file)
+            try:
+                if file[:5] != "video" and os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                print(e)
+        """
+        if filtered:
+            img = self.img_filtered
+        else:
+            img = self.img_unfiltered
 
+        # generating new pngs, mp4
+        for i in range(len(img)):
+            if plane == "z":
+                plt.imshow(img[i], cmap=cm.Greys_r)
+            elif plane == "y":
+                plt.imshow(img[:,i,:], cmap=cm.Greys_r)
+            elif plane == "x":
+                plt.imshow(img[:,:,i], cmap=cm.Greys_r)
+            plt.savefig('output/file%02d.png' % i)
+        os.chdir('output')
+        subprocess.call([
+            'ffmpeg', '-framerate', '8', '-i', 'file%02d.png', '-r', '30', '-pix_fmt', 'yuv420p',
+            'video%d.mp4' % vid
+        ])
+
+        # i think this is supposed to remove files but it does not.
+        for file_name in glob.glob("*.png"):
+            os.remove(file_name)
+        os.chdir('..')
 
 
 def low_pass_filter(img):
@@ -168,42 +156,25 @@ def low_pass_filter(img):
     # plt.imshow(img_back, cmap='gray', interpolation='nearest')
 
 
-# Cross section testing
-# TODO: make function
 
-#img_test = Img3D(200, 15, 1)
-#plt.imshow(img_test.img_filtered[:, 200//2, :], cmap='gray', interpolation='nearest')
-#x_section = img_test.img_filtered[:, 200//2, :][:, 200//2]
-#plt.plot(range(200), x_section)   #/np.linalg.norm(x_section)
-#plt.show()
+img_test = Img3D(150, 1, 6)
 
+start = time.time()
 
-img_test = Img3D(200, 8, 3)
-x, y, z = img_test.centers[0]
-#plt.imshow(img_test.img_filtered[:, y, :], cmap='gray', interpolation='nearest')
-x_section = img_test.img_filtered[:, y, :][:, x]
-plt.plot(range(200), x_section)   #/np.linalg.norm(x_section)
-plt.show()
-
-#start = time.time()
-
-#generate_video(img_test, 0, False, "xy")
-#generate_video(img_test, 1, False, "xz")
-#generate_video(img_test, 2, False, "yz")
+#img_test.generate_video(0, False, "z")
+#img_test.generate_video(1, False, "y")
+#img_test.generate_video(2, False, "x")
 
 print("generating videos")
-#generate_video(img_test, 0, True, "xy")
-#generate_video(img_test, 1, True, "xz")
-#generate_video(img_test, 2, True, "yz")
 
-#generate_video_2(low_pass_filter(img_test.img_unfiltered), 0, "xy")
+img_test.generate_video(3, True, "z")
+img_test.generate_video(4, True, "y")
+img_test.generate_video(5, True, "x")
 
-#end = time.time()
-#print("time elapsed: %d" % (end - start))
+end = time.time()
 
-# TODO: do I want to do this in every direction?
+print("time elapsed: %f min" % ((end - start)/60.0))
 
-"""
-for each center, create subplot:
+print("making cross section...")
 
-"""
+img_test.x_section()
